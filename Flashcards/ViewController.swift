a471e32df6a5a5db0a2d9470e978030d72588934
maoki109 <mariaoki109@gmail.com>
2020-02-15 12:11:21 -0700

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Mari Aoki on 2/15/20.
+//  Copyright © 2020 Mari Aoki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
